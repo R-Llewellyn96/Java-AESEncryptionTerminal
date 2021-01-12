e9fe2d2c45eb50858134a0df9419b0a5207112ad
Ryan Llewellyn <ryanz.llewellyn@gmail.com>
2021-01-12 04:50:52 +0000

    Separation of key generation method.

--- src/com/company/AESEncrypt.java
+++ src/com/company/AESEncrypt.java
@@ -7,13 +7,15 @@ package com.company;
  * Purpose: perform AES Encryption and Decryption.
  */
 
-// import java security libraries
 
+// import java crypto libraries
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
+
+// import java security libraries
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -21,103 +23,100 @@ import java.security.spec.InvalidKeySpecException;
 
 // Main class for performing AES encryption and decryption
 public class AESEncrypt {
-	
-	// Main method, used for calling methods
-	public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, InvalidAlgorithmParameterException, BadPaddingException, IllegalBlockSizeException, InvalidKeySpecException {
-		
+
+	// Method Caller
+	public static void methodCaller(SecretKey key, String encryptionAlgorithm, String message) throws NoSuchPaddingException, InvalidKeyException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
+
+		// Generate Initialisation Vector
+		IvParameterSpec ivParamSpec = UtilAES.generateIv();
+
+		// Generate Cipher-text using encryption
+		String cipherText = UtilAES.encrypt(encryptionAlgorithm, message, key, ivParamSpec);
+
+		// Decrypt Cipher-text into plain text
+		String plainText = UtilAES.decrypt(encryptionAlgorithm, cipherText, key, ivParamSpec);
+
+		// Convert key to string to allow for display &
+		// Print AES key being used to terminal
+		System.out.println("AES Symmetric key used : " + UtilAES.secretKeyToString(key));
+
+		// Print input message
+		System.out.println("Input message          : " + message);
+
+		// Print cipher text
+		System.out.println("Encrypted Ciphertext   : " + cipherText);
+
+		// Print decoded plaintext
+		System.out.println("Decrypted Ciphertext   : " + plainText);
+	}
+
+	// Return key generated using password based inputs
+	public static SecretKey passwordBasedInputs() throws InvalidKeySpecException, NoSuchAlgorithmException {
+
+		// Define password to generate key from
+		final String password = "password123";
+
+		// Define salt to apply to password before hashing
+		final String salt = "12345";
+
+		// Generate secure key using password return key to method caller
+		return UtilAES.getKeyFromPassword(password, salt);
+	}
+
+	// Return key based on key generation method selected by user
+	public static SecretKey keyMethodSelection(int keyGenMethod) throws NoSuchAlgorithmException, InvalidKeySpecException {
+
+		// User has selected to generate key from provided password
+		if (keyGenMethod == 1) {
+
+			// Get password based encryption inputs, generate key and return to caller
+			return passwordBasedInputs();
+
+			// User has selected to provide their own key
+		} else if (keyGenMethod == 2) {
+
+			// Define user input key
+			String userInputKey = "5EMkroyxam/w+pseq8anu1yXTUPmXfMDG/H8TPr9S5w=";
+
+			// Return user input key to caller
+			return UtilAES.stringToSecretKey(userInputKey);
+
+			// if no selection made, default to random AES key generation
+		} else {
+
+			// Generate secure random key for AES
+			return UtilAES.generateKey(256);
+		}
+	}
+
+	// Get message for encryption from user input prompt and return to caller
+	public static String getUserMessageInput(){
+
 		// Define User input message
 		String message = "Testing of AES CBC and PKCS5Padding, this is a test of JCE on this subject"
 				+ "matter and an evaluation of the java crpytography extension for use as a text encryptor";
-		
+
+		// Return message string to caller
+		return message;
+	}
+	
+	// Main method, used for calling methods
+	public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, InvalidAlgorithmParameterException, BadPaddingException, IllegalBlockSizeException, InvalidKeySpecException {
+
 		// Define encryption algorithm to use
-		String EncryptionAlgorithm = "AES/CBC/PKCS5Padding";
-		
-		int keyGenMethod = 0;
-		
-		// User has selected to generate key from provided password
-		if (keyGenMethod == 1) {
-			
-			// Define password to generate key from			
-			final String password = "password123";
-						
-			// Define salt to apply to password before hashing
-			final String salt = "12345";
-			
-			// Generate secure key using password
-			final SecretKey keyPBE = UtilAES.getKeyFromPassword(password, salt);
-			
-			// Convert key to string to allow for display &
-			// Print AES key being used to terminal
-			System.out.println("AES Symmetric key used: " + UtilAES.secretKeyToString(keyPBE));
-			
-			// Generate Initialisation Vector
-			IvParameterSpec ivParamSpec = UtilAES.generateIv();
-			
-			// Generate Cipher-text using encryption
-			String cipherText = UtilAES.encrypt(EncryptionAlgorithm, message, keyPBE, ivParamSpec);
-			
-			// Decrypt Cipher-text into plain text
-			String plainText = UtilAES.decrypt(EncryptionAlgorithm, cipherText, keyPBE, ivParamSpec);
-			
-			System.out.println("Input message: " + message);
-			
-			System.out.println("Encrypted Ciphertext: " + cipherText);
-			
-			System.out.println("Decrypted Ciphertext: " + plainText);
-		
-		// User has selected to provide their own key
-		} else if (keyGenMethod == 2) {
-			
-			String userInputKey = "5EMkroyxam/w+pseq8anu1yXTUPmXfMDG/H8TPr9S5w=";
-			
-			// Convert input string from user to use as key for encryption
-			final SecretKey keyUser = UtilAES.stringToSecretKey(userInputKey);
-			
-			// Convert key to string to allow for display &
-			// Print AES key being used to terminal
-			System.out.println("AES Symmetric key used: " + UtilAES.secretKeyToString(keyUser));
-			
-			// Generate Initialisation Vector
-			IvParameterSpec ivParamSpec = UtilAES.generateIv();
-			
-			// Generate Cipher-text using encryption
-			String cipherText = UtilAES.encrypt(EncryptionAlgorithm, message, keyUser, ivParamSpec);
-			
-			// Decrypt Cipher-text into plain text
-			String plainText = UtilAES.decrypt(EncryptionAlgorithm, cipherText, keyUser, ivParamSpec);
-			
-			System.out.println("Input message: " + message);
-			
-			System.out.println("Encrypted Ciphertext: " + cipherText);
-			
-			System.out.println("Decrypted Ciphertext: " + plainText);
-			
-		// if no selection made, default to random AES key generation
-		} else {
-			
-			// Generate secure random key for AES
-			final SecretKey keyRandom = UtilAES.generateKey(256);
-			
-			// Convert key to string to allow for display &
-			// Print AES key being used to terminal
-			System.out.println("AES Symmetric key used: " + UtilAES.secretKeyToString(keyRandom));
-			
-			// Generate Initialisation Vector
-			IvParameterSpec ivParamSpec = UtilAES.generateIv();
-			
-			// Generate Cipher-text using encryption
-			String cipherText = UtilAES.encrypt(EncryptionAlgorithm, message, keyRandom, ivParamSpec);
-			
-			// Decrypt Cipher-text into plain text
-			String plainText = UtilAES.decrypt(EncryptionAlgorithm, cipherText, keyRandom, ivParamSpec);
-			
-			System.out.println("Input message: " + message);
-			
-			System.out.println("Encrypted Ciphertext: " + cipherText);
-			
-			System.out.println("Decrypted Ciphertext: " + plainText);
-			
-		}
+		String encryptionAlgorithm = "AES/CBC/PKCS5Padding";
+
+		// Select key generation method
+		int keyGenMethod = 2;
+
+		// Get user input message after validation
+		String message = getUserMessageInput();
+
+		// Get key from key generation method selected
+		SecretKey key = keyMethodSelection(keyGenMethod);
+
+		// Begin Message encryption using generated key, encryption algorithm and message
+		methodCaller(key, encryptionAlgorithm, message);
 
 	// End of main method
 	}
--- src/com/company/UtilAES.java
+++ src/com/company/UtilAES.java
@@ -25,122 +25,104 @@ import java.util.Base64;
 public class UtilAES {
 
 	// Generate Secret Key for AES, Symmetric key used for encryption and decryption
-		public static SecretKey generateKey(int n) throws NoSuchAlgorithmException {
-		    
-			// Create key generator object and define instance as AES Symmetric Key
-			KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+	public static SecretKey generateKey(int n) throws NoSuchAlgorithmException {
+
+		// Create key generator object and define instance as AES Symmetric Key
+		KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
 			
-			// Initialise key generator
-		    keyGenerator.init(n);
+		// Initialise key generator
+		keyGenerator.init(n);
 		    
-		    // Generate key and assign value to key
-		    SecretKey key = keyGenerator.generateKey();
-		    
-		    // Return key object to method caller
-		    return key;
-		}
+		// Generate key and assign value to key and return key object to method caller
+		return keyGenerator.generateKey();
+	}
 		
-		// Generate Secret Key from user given password, note this is much less secure than random generation
-		public static SecretKey getKeyFromPassword(String password, String salt)
+	// Generate Secret Key from user given password, note this is much less secure than random generation
+	public static SecretKey getKeyFromPassword(String password, String salt)
 				throws NoSuchAlgorithmException, InvalidKeySpecException {
 			
-			// Create key generator object and define instance as Password Based Encryption with SHA256 hashing
-			SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+		// Create key generator object and define instance as Password Based Encryption with SHA256 hashing
+		SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
 			
-			// Define key specifications for password based encryption, convert password to byte array, 
-			// apply salt and use SHA256 to hash the result
-			KeySpec spec = new PBEKeySpec(password.toCharArray(), salt.getBytes(), 65536, 256);
+		// Define key specifications for password based encryption, convert password to byte array,
+		// apply salt and use SHA256 to hash the result
+		KeySpec spec = new PBEKeySpec(password.toCharArray(), salt.getBytes(), 65536, 256);
 		    
-			// Generate secret key using PBKDF2 algorithm and specifications defined above
-			SecretKey key = new SecretKeySpec(factory.generateSecret(spec).getEncoded(), "AES");
-		    
-			// Return secret key object to method caller
-		    return key;    
-		}
+		// Generate secret key using PBKDF2 algorithm and specifications defined above and
+		// return secret key object to method caller
+		return new SecretKeySpec(factory.generateSecret(spec).getEncoded(), "AES");
+	}
 		
-		// AES Requires an initialisation vector in CBC mode, 
-		// this method generates an initialisation vector in a securely random way
-		public static IvParameterSpec generateIv() {
+	// AES Requires an initialisation vector in CBC mode,
+	// this method generates an initialisation vector in a securely random way
+	public static IvParameterSpec generateIv() {
 			
-			// Define initialisation vector as a 16 bit byte
-		    byte[] iv = new byte[16];
+		// Define initialisation vector as a 16 bit byte
+		byte[] iv = new byte[16];
 		    
-		    // Generate random byte string in a secure way
-		    new SecureRandom().nextBytes(iv);
+		// Generate random byte string in a secure way
+		new SecureRandom().nextBytes(iv);
 		    
-		    IvParameterSpec initialisationVector = new IvParameterSpec(iv);
-		    
-		    // Return initialisation vector to method caller
-		    return initialisationVector;
-		}
+		// Return initialisation vector to method caller
+		return new IvParameterSpec(iv);
+	}
 		
-		// Begin AES Encryption using algorithm, message, key and initialisation vector
-		public static String encrypt(String algorithm, String message, SecretKey key,
+	// Begin AES Encryption using algorithm, message, key and initialisation vector
+	public static String encrypt(String algorithm, String message, SecretKey key,
 			    IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException,
 			    InvalidAlgorithmParameterException, InvalidKeyException,
 			    BadPaddingException, IllegalBlockSizeException {
 			
-			// Define cipher being used
-			Cipher cipher = Cipher.getInstance(algorithm);
+		// Define cipher being used
+		Cipher cipher = Cipher.getInstance(algorithm);
 			
-			// Initialise cipher for encryption
-			cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+		// Initialise cipher for encryption
+		cipher.init(Cipher.ENCRYPT_MODE, key, iv);
 			
-			// Define cipher-text as encrypted input string 
-			byte[] cipherText = cipher.doFinal(message.getBytes());
+		// Define cipher-text as encrypted input string
+		byte[] cipherText = cipher.doFinal(message.getBytes());
 			
-			// Encode byte array of cipher-text to a string
-			String cipherTextString = Base64.getEncoder().encodeToString(cipherText);
-			
-			// Return cipher-text as a string object to method caller
-			return cipherTextString;	
-		}
+		// Encode byte array of cipher-text to a string and return cipher-text as
+		// a string object to method caller
+		return Base64.getEncoder().encodeToString(cipherText);
+	}
 		
-		// Begin AES Decryption using algorithm, cipher-text, key and initialisation vector
-		public static String decrypt(String algorithm, String cipherText, SecretKey key,
+	// Begin AES Decryption using algorithm, cipher-text, key and initialisation vector
+	public static String decrypt(String algorithm, String cipherText, SecretKey key,
 			    IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException,
 			    InvalidAlgorithmParameterException, InvalidKeyException,
 			    BadPaddingException, IllegalBlockSizeException {
 			    
-			// Define cipher being used
-			Cipher cipher = Cipher.getInstance(algorithm);
+		// Define cipher being used
+		Cipher cipher = Cipher.getInstance(algorithm);
 			
-			// Initialise cipher for decryption
-			cipher.init(Cipher.DECRYPT_MODE, key, iv);
+		// Initialise cipher for decryption
+		cipher.init(Cipher.DECRYPT_MODE, key, iv);
 			
-			// Define plain-text as decrypted byte array of cipher-text
-			byte[] plainText = cipher.doFinal(Base64.getDecoder().decode(cipherText));
-			
-			// Convert byte array to a standard string object
-			String plainTextString = new String(plainText);
+		// Define plain-text as decrypted byte array of cipher-text
+		byte[] plainText = cipher.doFinal(Base64.getDecoder().decode(cipherText));
 			   
-			// Return plain text as a string object to method caller
-			return plainTextString;	
-		}
+		// Convert byte array to a standard string object and return plain text as
+		// a string object to method caller
+		return new String(plainText);
+	}
 		
-		// Take string and convert to secret key object
-		public static SecretKey stringToSecretKey(String keyString) {
+	// Take string and convert to secret key object
+	public static SecretKey stringToSecretKey(String keyString) {
 			
-			// Decode the base 64 encoded key string
-			byte[] decodedKey = Base64.getDecoder().decode(keyString);
+		// Decode the base 64 encoded key string
+		byte[] decodedKey = Base64.getDecoder().decode(keyString);
 			
-			// Convert decoded byte array key string into AES Secret Key
-			SecretKey key = new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");
-			
-			// Return secret key string as a secret key object to method caller
-			return key;
-			
-		}
+		// Return secret key string as a secret key object to method caller
+		return new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");
+	}
 		
-		// Take secret key object and convert to string
-		public static String secretKeyToString(SecretKey key) {
+	// Take secret key object and convert to string
+	public static String secretKeyToString(SecretKey key) {
 			
-			// Take encoded secret key and convert to standard string
-			String keyString = Base64.getEncoder().encodeToString(key.getEncoded());
-			
-			// Return secret key as a string to method caller
-			return keyString;
-		}
+		// Return secret key as a string to method caller
+		return Base64.getEncoder().encodeToString(key.getEncoded());
+	}
 		
 // End of class
 }
